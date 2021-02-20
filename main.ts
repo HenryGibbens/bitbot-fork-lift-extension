90e56325f04cef729c0bf5426e68cb2ddbe4849b
HenryGibbens <79278548+HenryGibbens@users.noreply.github.com>
2021-02-20 13:05:57 +0000

    change led

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-function left () {
-    bitbot.move(BBMotor.Left, BBDirection.Forward, 60)
-}
 function stop () {
     bitbot.setLedColor(0xFF0000)
     bitbot.stop(BBStopMode.Brake)
@@ -8,26 +5,32 @@ function stop () {
     bitbot.ledClear()
 }
 function RIGHT () {
-    bitbot.move(BBMotor.Right, BBDirection.Forward, 60)
+    bitbot.driveTurnMilliseconds(BBRobotDirection.Right, 20, 400)
+}
+function _let () {
+    bitbot.driveTurnMilliseconds(BBRobotDirection.Left, 20, 400)
 }
 radio.onReceivedString(function (receivedString) {
     bitbot.ledClear()
     if (receivedString == "start") {
-        start()
+        bitbot.setPixelColor(11, 0x8000FF)
     } else if (receivedString == "let") {
-        left()
+        bitbot.setPixelColor(10, 0x0080FF)
     } else if (receivedString == "right") {
-        bitbot.setPixelColor(0, 0x659900)
-        RIGHT()
+        bitbot.setPixelColor(7, 0xB09EFF)
+    } else if (receivedString == "up") {
+        bitbot.setPixelColor(2, 0xFF0000)
+    } else if (receivedString == "down") {
+        bitbot.setPixelColor(3, 0x18E600)
     } else {
         if (receivedString == "stop") {
-            stop()
+            bitbot.setPixelColor(8, 0x0000FF)
         }
     }
 })
 function start () {
     bitbot.setLedColor(0x18E600)
-    bitbot.go(BBDirection.Forward, 50)
+    bitbot.go(BBDirection.Forward, 20)
 }
 radio.setGroup(123)
 basic.forever(function () {
