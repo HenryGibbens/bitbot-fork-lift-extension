637f148e8a89ad0f2fb15d0969d09efeb911d077
HenryGibbens <79278548+HenryGibbens@users.noreply.github.com>
2021-02-20 13:26:52 +0000

    made some new things

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
+function up_lift () {
+    bitbot.bb360Servo(BBServos.P1, BBDirection.Forward, 50)
+}
 function stop () {
+    bitbot.bbStopServos()
     bitbot.setLedColor(0xFF0000)
     bitbot.stop(BBStopMode.Brake)
     basic.pause(1000)
     bitbot.ledClear()
+}
+function down_lift () {
+    bitbot.bb360Servo(BBServos.P1, BBDirection.Reverse, 50)
 }
 function RIGHT () {
     bitbot.driveTurnMilliseconds(BBRobotDirection.Right, 20, 400)
@@ -18,10 +25,10 @@ radio.onReceivedString(function (receivedString) {
         bitbot.setPixelColor(10, 0x0080FF)
     } else if (receivedString == "right") {
         bitbot.setPixelColor(7, 0xB09EFF)
-    } else if (receivedString == "up") {
-        bitbot.setPixelColor(2, 0xFF0000)
-    } else if (receivedString == "down") {
-        bitbot.setPixelColor(3, 0x18E600)
+    } else if (receivedString == "forklift down") {
+        bitbot.setPixelColor(9, 0xFF8000)
+    } else if (receivedString == "forklift up") {
+        bitbot.setPixelColor(5, 0xD82600)
     } else {
         if (receivedString == "stop") {
             bitbot.setPixelColor(8, 0x0000FF)
